753166d58715def3dad0dedf8710a5401a34cdc7
yRomulo <romulo.marroso@gmail.com>
2024-10-29 20:11:59 -0300

    aaaa

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aaaaaaa</Text>
+      <Text>tttttt</Text>
       <StatusBar style="auto" />
     </View>
   );
