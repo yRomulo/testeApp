253e08d542958ebbf593cfb53d511f3a98f27f49
João Pedro <moreirajp0707@gmail.com>
2024-10-29 19:52:03 -0300

    te

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
+      <Text>te</Text>
       <StatusBar style="auto" />
     </View>
   );
