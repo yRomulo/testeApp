06ec81f3c0abc3c5cce59ebfb127a5b9732f0c25
yRomulo <romulo.marroso@gmail.com>
2024-10-29 20:04:52 -0300

    do neida

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
+      <Text>aaaaaaa</Text>
       <StatusBar style="auto" />
     </View>
   );
